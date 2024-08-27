b3d459c1fbd07d29a501dea584186a625f49cecb
Mr.shipalkar <ashwinshipalkar1@gmail.com>
2024-08-27 19:23:04 +0530

    deployment ready

--- QuizzieAPP/src/pages/QuizInterface/QuizInterface.jsx
+++ QuizzieAPP/src/pages/QuizInterface/QuizInterface.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import './QuizInterface.css';
-import Result from '../../assets/Result.png';
+import Result from '../../assets/result.png';
 import API_URL from '../../services/config'
 
 function QuizInterface() {
